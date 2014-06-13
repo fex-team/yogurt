9e5a079133172262b8f50661cf30be285a674806
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-13 12:43:50 +0800

    first commit

---
+++ yogurt.js
@@ -0,0 +1,110 @@
+var fis = module.exports = require('fis');
+
+// 让 jello 打头的先加载。
+fis.require.prefixes.unshift('yogurt');
+
+fis.cli.name = 'yogurt';
+fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+
+fis.config.merge({
+    statics: '/public',
+
+    server: {
+        clean: {
+            exclude: "controllers/*,config/config.json,config/development.json,locales/*,models/*,server.js"
+        }
+    },
+
+    modules: {
+
+        parser: {
+            less: 'less',
+            sass: 'sass',
+            scss: 'sass',
+            tmpl: 'bdtmpl',
+            po: 'po'
+        },
+
+        preprocessor: {
+            tpl: 'extlang'
+        },
+
+        postprocessor: {
+            tpl: 'require-async',
+            js: 'jswrapper, require-async'
+        }
+    },
+
+    roadmap: {
+        ext: {
+            less: 'css',
+            sass: 'css',
+            scss: 'css',
+            tmpl: 'js',
+            po: 'json'
+        },
+
+        path: [
+
+            {
+                reg: /^\/widget\/(.*\.tpl)$/i,
+                isMod: true,
+                release: '/views/${namespace}/widget/$1'
+            },
+
+            {
+                reg: /^\/widget\/(.*\.(js|css))$/i,
+                isMod: true,
+                release: '${statics}/${namespace}/widget/$1'
+            },
+
+            {
+                reg: /^\/page\/(.+\.tpl)$/i,
+                isMod: true,
+                release: '/views/${namespace}/$1',
+                extras: {
+                    isPage: true
+                }
+            },
+
+            {
+                reg: /^\/(static)\/(.*)/i,
+                release: '${statics}/${namespace}/$2'
+            },
+
+            {
+                reg: /^\/(test)\/(.*)/i,
+                isMod: false,
+                release: '/$1/${namespace}/$2'
+            },
+
+            {
+                reg: 'server.conf',
+                release: '/config/server.conf'
+            },
+
+            {
+                reg: "build.sh",
+                release: false
+            },
+
+            {
+                reg: '${namespace}-map.json',
+                release: '/config/${namespace}-map.json'
+            },
+
+            {
+                reg: /^.+$/,
+                release: '${statics}/${namespace}$&'
+            }
+        ]
+    },
+
+    settings: {
+        postprocessor: {
+            jswrapper: {
+                type: 'amd'
+            }
+        }
+    }
+});
